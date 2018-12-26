4aa64f57ed1c3a1c2e5a5fb901569af4353ba8ca
Katie Duane <katiejduane@gmail.com>
2018-12-26 11:53:43 -0500

    searches refined and CSS improved, still a few tweaks to be made

--- wordWeb.js
+++ wordWeb.js
@@ -1,3 +1,8 @@
+ //to-do list:
+ //drop-down menu in search bar that displays recent searches
+ //display word relationship types with results below in columns?
+ 
+ 
 const apiKey = '9495b8cb95dffb0d23a251c3d28d2f9a';
 const baseUrl = `https://words.bighugelabs.com/api/2/${apiKey}/`
 
@@ -5,7 +10,7 @@ const baseUrl = `https://words.bighugelabs.com/api/2/${apiKey}/`
 $('#word-form').submit((event) => {
     //stops the browser from going forward
     event.preventDefault();
-    const wordSearch = $('#search-input').val();
+    let wordSearch = $('#search-input').val();
     // localStorage.setItem('movieList', movieSearch)
     console.log(wordSearch);
     const wordSearchUrl = `https://words.bighugelabs.com/api/2/${apiKey}/${wordSearch}/json`
@@ -14,21 +19,45 @@ $('#word-form').submit((event) => {
         console.log(wordInfo)
         for (let key in wordInfo) {
             newHTML += 
-            `<div class="wordType">
+            `
+            <div class="wordType">
             ${key}</div>
             <div class="relBox">`
             for (let key2 in wordInfo[key]) {
-                newHTML += `<div class="wordRel">
-                ${key2}</div>`
-                let wordArray = wordInfo[key][key2]
-                for (let i = 0; i < wordArray.length; i++) {
-                    newHTML += `<div class="relResults">${wordArray[i]}</div>`
+                if (key2 == 'syn') {
+                    newHTML += `<div class="wordRel">
+                    Synonyms</div>`
                 }
+                if (key2 == 'ant') {
+                    newHTML += `<div class="wordRel">
+                    Antonyms</div>`
+                }
+                if (key2 == 'sim') {
+                    newHTML += `<div class="wordRel">
+                    Similar Words</div>`
+                }
+                if (key2 == 'usr') {
+                    newHTML += `<div class="wordRel">
+                    User Suggestions</div>`
+                }
+                if (key2 == 'rel') {
+                    newHTML += `<div class="wordRel">
+                    Related Terms</div>`
+                }
+              
+                    let wordArray = wordInfo[key][key2]
+                    for (let i = 0; i < wordArray.length; i++) {
+                        if (i < wordArray.length - 1) {
+                        newHTML += `<span class="relResults">${wordArray[i]}, </span>`
+                        } else if (i == wordArray.length - 1) {
+                            newHTML += `<span class="relResults">${wordArray[i]} </span>`
+                        }
+                    }
             }
             newHTML += `</div>`
         } 
         
-        $('.results-box').html(newHTML)
+        $('.word-info-box').html(newHTML)
     }) 
 })
 
